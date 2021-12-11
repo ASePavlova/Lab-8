0dcc826c1ee22b9851441a5585b8b6b04362501a
ASePavlova <92172368+ASePavlova@users.noreply.github.com>
2021-12-11 18:23:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 # Лаботрная работа 8
 # Написать функцию, которая находит n-ый по порядку убывания элемент с списке без сортировки
+def kbig(nums, u):
+    if u==1:
+        return max(nums)
+    z = nums[0]
+    r1=max(nums)
+    nums.remove(r1)
+
+    for y in range(0, len(nums)):
+
+        for w in range(0, len(nums)):
+
+            if abs(r1 - nums[w]) < z:
+                z = abs(r1 - nums[w])
+                el = nums[w]
+
+        z=nums[0]
+
+        r1 = el
+        nums.remove(r1)
+
+        if y+2 == u:
+            return r1
